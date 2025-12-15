e51d81ebe442411541f2622eaa76874e27750d3d
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-12-15 13:48:14 +0100

    Threads  + Exceptions - 2

--- threads-exceptions/thread_exceptions.cpp
+++ threads-exceptions/thread_exceptions.cpp
@@ -4,11 +4,79 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <variant>
 #include <vector>
 
 using namespace std::literals;
 
-void background_work(size_t id, const std::string& text, char& result, std::exception_ptr& eptr)
+template <typename... Ts>
+struct overloaded : Ts...
+{
+    using Ts::operator()...;
+};
+
+// deduction guide - C++17 - implicitly provided since C++20
+// template <typename... Ts>
+// overloaded(Ts...) -> overloaded<Ts...>;
+
+template <typename T>
+class Result
+{
+    // T value_;
+    // std::exception_ptr eptr_;
+    std::variant<std::monostate, T, std::exception_ptr> value_;
+
+public:
+    void set_exception(std::exception_ptr eptr)
+    {
+        value_ = eptr;
+    }
+
+    template <typename TValue>
+    void set_value(TValue&& value)
+    {
+        value_ = std::forward<TValue>(value);
+    }
+
+    T get()
+    {
+        // struct ValueVisitor
+        // {
+        //     T operator()(const T& v) const
+        //     {
+        //         return v;
+        //     }
+
+        //     T operator()(std::exception_ptr e) const
+        //     {
+        //         std::rethrow_exception(e);
+        //     }
+
+        //     T operator()(std::monostate) const
+        //     {
+        //         throw std::logic_error("Result/Exception is not set");
+        //     }
+        // };
+
+        // ValueVisitor value_visitor;
+        // return std::visit(value_visitor, value_);
+
+        auto value_visitor = overloaded{
+            [](const T& v) -> T {
+                return v;
+            },
+            [](std::exception_ptr e) -> T {
+                std::rethrow_exception(e);
+            },
+            [](std::monostate) -> T {
+                throw std::logic_error("Result/Exception is not set");
+            }};
+
+        return std::visit(value_visitor, value_);
+    }
+};
+
+void background_work(size_t id, const std::string& text, Result<char>& result)
 {
     try
     {
@@ -21,13 +89,13 @@ void background_work(size_t id, const std::string& text, char& result, std::exce
             std::this_thread::sleep_for(100ms);
         }
 
-        result = text.at(5); // potential exception
+        result.set_value(text.at(5)); // potential exception
 
         std::cout << "bw#" << id << " is finished..." << std::endl;
     }
     catch (...)
     {
-        eptr = std::current_exception();
+        result.set_exception(std::current_exception());
         return;
     }
 }
@@ -39,35 +107,28 @@ int main()
 
     const size_t no_of_threads = 2;
 
-    std::vector<char> results(2);
-    std::vector<std::exception_ptr> exceptions(2);
+    std::vector<Result<char>> results(no_of_threads);
 
     {
-        std::jthread thd_1{&background_work, 1, text, std::ref(results[0]), std::ref(exceptions[0])};
-        std::jthread thd_2{&background_work, 2, "bad", std::ref(results[1]), std::ref(exceptions[1])};
+        std::jthread thd_1{&background_work, 1, text, std::ref(results[0])};
+        std::jthread thd_2{&background_work, 2, "bad", std::ref(results[1])};
     } // implicit join
 
     for (size_t i = 0; i < no_of_threads; ++i)
     {
-        if (exceptions[i])
+        try
         {
-            try
-            {
-                std::rethrow_exception(exceptions[i]);
-            }
-            catch (const std::out_of_range& e)
-            {
-                std::cout << "Out of range caught!!! ";
-                std::cout << "Error caught: " << e.what() << std::endl;
-            }
-            catch (const std::exception& e)
-            {
-                std::cout << "Error caught: " << e.what() << std::endl;
-            }
+            char result = results[i].get();
+            std::cout << "result_" << std::to_string(i) << ": " << result << std::endl;
         }
-        else
+        catch (const std::out_of_range& e)
         {
-            std::cout << "result_" << std::to_string(i) << results[0] << std::endl;
+            std::cout << "Out of range caught!!! ";
+            std::cout << "Error caught: " << e.what() << std::endl;
+        }
+        catch (const std::exception& e)
+        {
+            std::cout << "Error caught: " << e.what() << std::endl;
         }
     }
 
